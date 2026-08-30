b0209d79d1b0c4610a14dd19b83fbfa2a1f72c71
nguyenvanphucthinhlvt28122007-svg <nguyen.van.phuc.thinh.lvt.28122007@gmail.com>
2026-08-30 11:11:47 +0700

    Update build_images.sh

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -2,28 +2,57 @@
 
 CHROOT=${CHROOT=$(pwd)/rootfs}
 
-#package rootfs
-rm -f rootfs.raw boot.raw
-mkdir -p files mnt
+# Dọn dẹp môi trường cũ
+rm -f files/emmc_full_raw.bin
+mkdir -p mnt_boot mnt_rootfs files
 
-# create boot
-truncate -s 67108864 boot.raw
-mkfs.ext2 boot.raw
-mount boot.raw mnt
-tar xf rootfs.tgz -C mnt ./boot --exclude='./boot/linux.efi' --strip-components=2
-umount mnt
+# 1. Tạo file đĩa ảo nguyên khối (RAW) dung lượng 2GB
+echo "Tạo file RAW 2GB..."
+truncate -s 2G files/emmc_full_raw.bin
 
-# create root img
-truncate -s 1610612736 rootfs.raw
-mkfs.ext4 rootfs.raw
-mount rootfs.raw mnt
-tar xpf rootfs.tgz -C mnt --exclude='./boot/*' --exclude='./root/*' --exclude='./dev/*'
+# 2. Ghi bảng phân vùng GPT và chia phân vùng bắt đầu từ Block 0
+echo "Tạo bảng phân vùng GPT..."
+parted -s files/emmc_full_raw.bin mklabel gpt
 
-# install gt
-cp -a dist/* mnt
+# Phân vùng boot: 64MB (Từ 1MiB đến 65MiB)
+parted -s files/emmc_full_raw.bin mkpart primary ext2 1MiB 65MiB
 
-umount mnt
+# Phân vùng rootfs: Từ 65MiB đến hết 2GB
+parted -s files/emmc_full_raw.bin mkpart primary ext4 65MiB 100%
 
-# create sparse android images 
-img2simg rootfs.raw files/rootfs.bin
-img2simg boot.raw files/boot.bin
+# 3. Ánh xạ file RAW thành ổ cứng ảo để hệ điều hành nhận diện
+echo "Ánh xạ vào loop device..."
+LOOPDEV=$(sudo losetup -P -f --show files/emmc_full_raw.bin)
+
+BOOT_PART="${LOOPDEV}p1"
+ROOTFS_PART="${LOOPDEV}p2"
+
+# 4. Format các phân vùng
+echo "Định dạng các phân vùng (ext2 cho boot, ext4 cho rootfs)..."
+sudo mkfs.ext2 $BOOT_PART
+sudo mkfs.ext4 $ROOTFS_PART
+
+# 5. Mount và bung file rootfs.tgz vào đúng phân vùng
+echo "Đang bung dữ liệu..."
+sudo mount $BOOT_PART mnt_boot
+sudo mount $ROOTFS_PART mnt_rootfs
+
+# Bung boot
+sudo tar xf rootfs.tgz -C mnt_boot ./boot --exclude='./boot/linux.efi' --strip-components=2
+
+# Bung rootfs
+sudo tar xpf rootfs.tgz -C mnt_rootfs --exclude='./boot/*' --exclude='./root/*' --exclude='./dev/*'
+
+# Cài đặt gt (nếu có thư mục dist)
+if [ -d "dist" ]; then
+    sudo cp -a dist/* mnt_rootfs/
+fi
+
+# 6. Unmount, dọn dẹp và ngắt kết nối ổ đĩa ảo
+echo "Hoàn tất, đang gỡ kết nối..."
+sudo umount mnt_boot mnt_rootfs
+sudo losetup -d $LOOPDEV
+rmdir mnt_boot mnt_rootfs
+
+echo "Đã tạo thành công files/emmc_full_raw.bin!"
+echo "Hãy dùng file này chọn vào mục Emmc Data để nạp bằng Miko."
